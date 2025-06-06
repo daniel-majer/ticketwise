279d66f4ecaa8523be45e8ce2338324c78cd4787
daniel-majer <dano.majer@protonmail.com>
2025-06-06 20:22:21 +0200

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,59 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // 1. React-related packages.
+            ['^react', '^next'],
 
-export default eslintConfig;
+            // 2. Other external packages.
+            ['^@?\\w'],
+
+            // 3. Side effect imports (CSS, polyfills, ...).
+            ['^\\u0000'],
+
+            // 4. Absolute imports (based on tsconfig.json or jsconfig.json paths), e.g. @components, @utils.
+            ['^@components', '^@hooks', '^@utils', '^@lib', '^@styles'],
+
+            // 5. Relative imports: parent directories first.
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+
+            // 6. Relative imports: current directory.
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+            // 7. Style imports.
+            [
+              '\\.css$',
+              '\\.scss$',
+              '\\.sass$',
+              '\\.less$',
+              '\\.styl$',
+              '\\.tailwind$',
+            ],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
+]
+
+export default eslintConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <main className="text-5xl md:text-2xl"></main>
         {children}
       </body>
     </html>
